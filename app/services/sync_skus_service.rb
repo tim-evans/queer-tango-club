0b70df1e8e1b883ea3d80e69a82f7330eb266288
Tim Evans <tim.c.evans@me.com>
2015-12-12 20:23:05 -0500

    add some pseudocode for syncing SKUs with Stripe and Square

---
+++ app/services/sync_skus_service.rb
@@ -0,0 +1,38 @@
+class SyncSkusService
+  def intialize(event)
+    @event = event
+  end
+
+  def sync!
+    product = Stripe::Product.create(
+      name: @event.name,
+      active: true,
+      description: @event.description,
+      shippable: false,
+      url: '' # use event_url(@event)
+    )
+    sku = Stripe::SKU.create(
+      id: @event.sku,
+      product: product.id,
+      price: @event.ticket_cost,
+      currency: 'USD',
+      inventory: {
+        type: 'finite'
+        quantity: @event.max_attendees
+      }
+    )
+
+    Square::Item.create(
+      name: @event.name,
+      description: @event.description,
+      variations: [{
+        name: @event.name,
+        sku: @event.sku
+        price_money: {
+          currency_code: 'USD',
+          amount: @event.ticket_cost
+        }
+      }]
+    )
+  end
+end
