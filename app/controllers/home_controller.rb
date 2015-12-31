f5fed2722ccc7ac7d3722f89ec3c913a9a42016a
Tim Evans <tim.c.evans@me.com>
2015-12-31 11:41:37 -0500

    order upcoming events by when they occur

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
     @hero_src = ""
-    @upcoming = Event.where("starts_at >= ?", DateTime.now)
+    @upcoming = Event.where("starts_at >= ?", DateTime.now).order(starts_at: :asc)
   end
 end
