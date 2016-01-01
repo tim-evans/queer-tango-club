9c2f7e94b1eaa45cff7bc00a52a4e1674ed73d89
Tim Evans <tim.c.evans@me.com>
2016-01-01 18:35:41 -0500

    :lock: use relative urls to make the site have a green lock

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @hero_src = view_context.image_url('IMG_4024.jpg')
+    @hero_src = view_context.image_url('IMG_4024.jpg', protocol: :relative)
     @upcoming = Event.where('starts_at >= ?', DateTime.now).order(starts_at: :asc)
   end
 end
