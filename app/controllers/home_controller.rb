6685511cfe20a14dcbdef91c314c45d81237c831
Tim Evans <tim.c.evans@me.com>
2016-01-01 18:38:16 -0500

    :lock: ok; now make it secure

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,14 @@
 class HomeController < ApplicationController
+  def protocol
+    if Rails.env.production?
+      'https://'
+    else
+      :request
+    end
+  end
+
   def index
-    @hero_src = view_context.image_url('IMG_4024.jpg', protocol: :relative)
+    @hero_src = view_context.image_url('IMG_4024.jpg', protocol: protocol)
     @upcoming = Event.where('starts_at >= ?', DateTime.now).order(starts_at: :asc)
   end
 end
