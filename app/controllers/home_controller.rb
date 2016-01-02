9dd413fa81663db87c129908234d5f62e277c260
Tim Evans <tim.c.evans@me.com>
2016-01-01 19:10:43 -0500

    :sweat_smile: fixing the things

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,6 @@
 class HomeController < ApplicationController
-  def protocol
-    if Rails.env.production?
-      'https://'
-    else
-      :request
-    end
-  end
-
   def index
-    @hero_src = view_context.image_url('IMG_4024.jpg', protocol: protocol)
+    @hero_src = 'IMG_4024.jpg'
     @upcoming = Event.where('starts_at >= ?', DateTime.now).order(starts_at: :asc)
   end
 end
