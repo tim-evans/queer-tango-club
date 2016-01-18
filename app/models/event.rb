9a47700c246c071fa696dd6af7ec24b1c9a50618
Tim Evans <tim.c.evans@me.com>
2016-01-18 09:49:56 -0500

    lighten cover photos before converting to grayscale

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ActiveRecord::Base
-  has_attached_file :cover_photo, styles: { grayscale: { convert_options: '-colorspace Gray' } }
+  has_attached_file :cover_photo, styles: { grayscale: { convert_options: '-colorspace gray -gamma 1.1' } }
   validates_attachment_content_type :cover_photo, content_type: %w(image/jpeg image/jpg image/png)
 
   has_many :sessions
