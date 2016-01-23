51ede28f94d50701494e81436d554ddf26bf5096
Tim Evans <tim.c.evans@me.com>
2016-01-23 13:41:26 -0500

    handle multiple locations and hide nearby cafes / POIs by default

--- app/assets/javascripts/components/accordion.js
+++ app/assets/javascripts/components/accordion.js
@@ -5,9 +5,14 @@ $(function () {
   $element.each(function () {
     var id = $(this).data('accordion');
     var $el = $(this);
-    var height = $('#' + id).height();;
+    var height = $('#' + id).height();
     $('#' + id).css('overflow', 'hidden');
     var isLastChild = $('#' + id).index() === $(this).parent().children().length - 1;
+
+    if ($el.hasClass('collapsed')) {
+      isLastChild = false;
+      $('#' + id).css({ height: 0, opacity: 0, marginBottom: 0 });
+    }
 
     $(this).on('click', function () {
       $el.toggleClass('collapsed');
@@ -16,6 +21,10 @@ $(function () {
         var margin = isLastChild ? 50 : 0;
         $('#' + id).animate({ height: 0, opacity: 0, marginBottom: margin }, 200);
       } else {
+        var style = $('#' + id).attr('style');
+        $('#' + id).removeAttr('style');
+        height = $('#' + id).height();
+        $('#' + id).attr('style', style);
         $('#' + id).animate({ height: height, opacity: 1, marginBottom: 0 }, 200);
       }
     });
