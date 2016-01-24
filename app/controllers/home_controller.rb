922fa8807a699938695c55cce5114c0e214f8586
Tim Evans <tim.c.evans@me.com>
2016-01-24 10:51:32 -0500

    add new cover photo; handle yellow in logo

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @hero_src = 'IMG_4024.jpg'
+    @hero_src = 'IMG_3562.jpg'
     @upcoming = Event.where('starts_at >= ?', DateTime.now).order(starts_at: :asc)
   end
 end
