50900d51aa6a0c65417fdbc03584a7c68dbd8aa1
Tim Evans <tim.c.evans@me.com>
2016-02-08 23:12:03 -0500

    fix photo sizing

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,5 @@
 class Photo < ActiveRecord::Base
-  has_attached_file :attachment, styles: { fill: 'x500' }
+  has_attached_file :attachment, styles: { fill: '500x' }
   validates_attachment_content_type :attachment, content_type: %w(image/jpeg image/jpg image/png)
   belongs_to :event
   belongs_to :teacher
