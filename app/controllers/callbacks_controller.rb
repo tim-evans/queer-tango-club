1d53c5398a51036256e5464a547131fa6ecb6907
Tim Evans <tim.c.evans@me.com>
2016-02-27 12:09:51 -0500

    use google oauth to log in

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,6 +1,10 @@
 class CallbacksController < Devise::OmniauthCallbacksController
-  def facebook
+  def google_oauth2
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    sign_in_and_redirect @user
+    if @user
+      sign_in_and_redirect @user
+    else
+      redirect_to "/"
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable,
-         omniauth_providers: [:facebook]
+         omniauth_providers: [:google_oauth2]
 
   def self.from_omniauth(auth)
     users = where(provider: auth.provider, uid: auth.uid)
