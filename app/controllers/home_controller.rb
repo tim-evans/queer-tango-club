56dde6f5345c431f9d36525a0f6e893f342410ff
Tim Evans <tim.c.evans@me.com>
2016-02-27 20:29:42 -0500

    update home photo and theme color

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @hero_src = 'IMG_3562.jpg'
+    @hero_src = 'IMG_3986.png'
     @upcoming = Event.where('starts_at >= ?', DateTime.now).order(starts_at: :asc)
   end
 end
