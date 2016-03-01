2e520d24d6317286213e59ed5f2166f13702f969
Tim Evans <tim.c.evans@me.com>
2016-02-29 23:25:18 -0500

    implement full_title for upcoming events on the home page

--- app/models/cover_photo.rb
+++ app/models/cover_photo.rb
@@ -5,4 +5,16 @@ class CoverPhoto < ActiveRecord::Base
   validates_attachment_content_type :attachment, content_type: %w(image/jpeg image/jpg image/png)
 
   belongs_to :event
+
+  def full_title
+    if title
+      if title == event.title
+        title
+      else
+        "#{event.title}&mdash; #{title}".html_safe
+      end
+    else
+      event.title
+    end
+  end
 end
