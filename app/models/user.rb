032f50844a6209091d0d9ff5e5a6645ea471da58
Tim Evans <tim.c.evans@me.com>
2016-02-29 23:17:05 -0500

    display the Google Name of the user

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@ class User < ActiveRecord::Base
       users.first_or_create do |user|
         user.provider = auth.provider
         user.uid = auth.uid
+        user.name = auth.info.name
         user.email = auth.info.email
         user.password = Devise.friendly_token[0,20]
       end
