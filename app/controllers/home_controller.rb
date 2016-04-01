98b233d6a6dcb6bcc7a3731bb38727da04458fd4
Tim Evans <tim.c.evans@me.com>
2016-04-01 00:20:11 -0400

    update color and cover photo

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,7 @@ class HomeController < ApplicationController
     @hero_image = OpenStruct.new({
       width: 3147,
       height: 2098,
-      src: 'IMG_3986.png'
+      src: 'triple.jpg'
     })
     @upcoming = Event.where('ends_at >= ?', DateTime.now).order(starts_at: :asc)
   end
