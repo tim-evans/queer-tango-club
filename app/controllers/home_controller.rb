261af3d89bf8dc84667d825f5b702b980e632406
Tim Evans <tim.c.evans@me.com>
2016-05-03 00:05:21 -0400

    updates for may 2016

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,8 +4,8 @@ class HomeController < ApplicationController
   def index
     @hero_image = OpenStruct.new({
       width: 2048,
-      height: 1365,
-      src: 'triple.png'
+      height: 1867,
+      src: 'may2016.png'
     })
     @upcoming = Event.where('ends_at >= ?', DateTime.now).order(starts_at: :asc)
   end
