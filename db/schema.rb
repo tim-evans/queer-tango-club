234e9e9449dffa96e6d40be255e3b3669c0c41cc
Tim Evans <tim.c.evans@me.com>
2016-09-05 23:57:08 -0400

    add published column to events

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160906023509) do
+ActiveRecord::Schema.define(version: 20160906035340) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 20160906023509) do
     t.date     "ends_at"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.boolean  "published"
   end
 
   create_table "guests", force: :cascade do |t|
