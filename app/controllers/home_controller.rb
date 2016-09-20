121e571302061caf27dfe503ac3030e9840bdd80
Tim Evans <tim.c.evans@me.com>
2016-09-20 14:14:07 -0400

    update homepage image

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,8 +4,8 @@ class HomeController < ApplicationController
   def index
     @hero_image = OpenStruct.new({
       width: 2048,
-      height: 1867,
-      src: 'may2016.png'
+      height: 1152,
+      src: 'sep2016.png'
     })
     @upcoming = Event.published.upcoming.order(starts_at: :asc)
   end
