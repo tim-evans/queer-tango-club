8bf59b6a4565951969e8089a116cb8dc5ed3af24
Tim Evans <tim.c.evans@me.com>
2017-04-08 10:43:55 -0400

    fix mapbox geolocation to choose the most relevant location

--- app/services/mapbox_service.rb
+++ app/services/mapbox_service.rb
@@ -2,7 +2,7 @@ class MapboxService
   QUERY_URL = 'https://api.mapbox.com/geocoding/v5/mapbox.places'
 
   def geolocate(location)
-    query = "#{location.address_line}, #{location.city}, #{location.region_code} #{location.postal_code}"
+    query = "#{location.address_line}, #{location.city}, #{location.region_code}, #{location.postal_code}"
 
     places = Unirest.get("#{QUERY_URL}/#{query.tr(' ', '+')}.json?access_token=#{ENV['MAPBOX_TOKEN']}",
                          headers: {
@@ -10,6 +10,11 @@ class MapboxService
                            'Content-Type' => 'application/json'
                          }).body
 
-    places['features'][0]['center']
+    feature = places['features'].find do |feature|
+      feature['context'].find { |ctx| ctx['id'].start_with?('postcode') }['text'] ==
+        location.postal_code
+    end || places['features'][0]
+
+    feature['center']
   end
 end
