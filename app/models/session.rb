8124e5f93129fd773519f44640fe028aa01dc781
Tim Evans <tim.c.evans@me.com>
2018-03-14 14:07:56 -0700

    add beginner class signup

--- app/models/session.rb
+++ app/models/session.rb
@@ -16,7 +16,7 @@ class Session < ActiveRecord::Base
   monetize :ticket_cost, as: :cost, with_model_currency: :ticket_currency, allow_nil: true
 
   def registerable?
-    (event.id == 45 || event.id == 48) && DateTime.current < starts_at
+    (event.id == 45 || event.id == 46 || event.id == 48) && DateTime.current < starts_at
   end
 
   def highlight?
